e07c4da2a205dc83252a0e024772687c7b048aba
Emiljan Dusha <ext_edusha@ringana.com>
2026-05-14 09:06:50 +0200

    feat: created async-rust crate

---
+++ async-rust/target/debug/async-rust.d
@@ -0,0 +1,1 @@
+/home/ext_edusha/projects/rust-books-exercises/async-rust/target/debug/async-rust: /home/ext_edusha/projects/rust-books-exercises/async-rust/src/main.rs
---
+++ async-rust/target/debug/deps/async_rust-733dc66852a2e700.d
@@ -0,0 +1,5 @@
+/home/ext_edusha/projects/rust-books-exercises/async-rust/target/debug/deps/async_rust-733dc66852a2e700: src/main.rs
+
+/home/ext_edusha/projects/rust-books-exercises/async-rust/target/debug/deps/async_rust-733dc66852a2e700.d: src/main.rs
+
+src/main.rs:
